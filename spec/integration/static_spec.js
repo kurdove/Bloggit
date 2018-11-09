aa7fb0f3af2feb94f8084c2bfbf5ade9a78797d9
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-09 11:41:16 -0800

    assignment-01 complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe('routes : static', ()=>{
     describe('GET /', ()=>{
@@ -8,6 +9,15 @@ describe('routes : static', ()=>{
             request.get(base, (err, res, body)=>{
                 expect(res.statusCode).toBe(200);
                 done();
+            });
+        });
+    });
+
+    describe('GET /marco', ()=>{
+        it('should return status code 200 and --polo--', (done)=>{
+            request.get(marco, (err, res, body)=>{
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe('polo');
             });
         });
     });
