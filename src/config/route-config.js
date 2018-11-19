3f0e18e1985f7acbe398eaac69eab22d32992767
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-19 07:49:50 -0800

    checkpoint-09 complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,10 +2,12 @@ module.exports = {
     init(app){
       const staticRoutes = require("../routes/static");
       const topicRoutes = require("../routes/topics");
-      const advertisementRoutes = require("../routes/advertisement");
+      const advertisementRoutes = require("../routes/advertisement")
+      const postRoutes = require("../routes/posts");
 
       app.use(staticRoutes);
       app.use(topicRoutes);
       app.use(advertisementRoutes);
+      app.use(postRoutes);
     }
   }
