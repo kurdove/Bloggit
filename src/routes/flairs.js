9af346abb18e8960b8ee6e9e293578a4f80943ba
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-21 12:54:29 -0800

    assignment-10 debugging 01

--- src/routes/flairs.js
+++ src/routes/flairs.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const flairController = require("../controllers/flairController")
+const flairController = require("../controllers/flairController");
+const validation = require("./validation");
 
 router.get("/topics/:topicId/flairs/new", flairController.new);
-router.post("/topics/:topicId/flairs/create", flairController.create);
+router.post("/topics/:topicId/flairs/create", validation.validatePosts, flairController.create);
 router.get("/topics/:topicId/flairs/:id", flairController.show);
 router.post("/topics/:topicId/flairs/:id/destroy", flairController.destroy);
 router.get("/topics/:topicId/flairs/:id/edit", flairController.edit);
-router.post("/topics/:topicId/flairs/:id/update", flairController.update);
+router.post("/topics/:topicId/flairs/:id/update", validation.validatePosts, flairController.update);
 
 module.exports = router;
