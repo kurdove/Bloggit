030f0e95e505df7c674d0dffdc36f95866a96d9e
Evgheni Curdov <kurdov.e@gmail.com>
2018-11-26 19:54:03 -0800

    assignment-13-comments complete

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -129,6 +129,41 @@ describe("routes : comments", () => {
         });
     });
 
+    // Comments assignment: Member who doesn't own comment
+    describe('non-owner user performing CRUD actions on Comment', () => {
+        beforeEach((done) => {
+            request.get({
+                url: 'http://localhost:3000/auth/fake',
+                form: {
+                    userId: 2
+                }
+            }, (err, res, body) => {
+                done();
+            });
+        });
+      
+        describe('POST /topics/:topicId/:posts/:postId/comments/:id/destroy', () => {
+            it('should not delete another users comment', (done) => {
+                Comment.all()
+                .then((comments) => {
+                    const commentCountBeforeDelete= comments.length;
+
+                    expect(commentCountBeforeDelete).toBe(1);
+
+                    request.post( `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`, 
+                    (err, res, body) => {
+                        Comment.all()
+                        .then((comments) => {
+                            expect(err).toBeNull();
+                            expect(comments.length).toBe(commentCountBeforeDelete);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
     //signed in user
     describe("signed in user performing CRUD actions for Comment", () => {
 
@@ -200,6 +235,42 @@ describe("routes : comments", () => {
    
         });
    
+    });
+
+    // Comments assignment: Admin delete comment
+    describe('admin user performing CRUD actions on Comment', () => {
+        beforeEach((done) => {
+            request.get({
+                url: 'http://localhost:3000/auth/fake',
+                form: {
+                    role: 'admin',
+                    userId: this.user.id
+                }
+            }, (err, res, body) => {
+                done();
+            });
+        });
+        describe('POST /topics/:topicId/posts/:postId/comments/:id/destroy', () => {
+            it('should delete the comment with the associated ID', (done) => {
+                Comment.all()
+                .then((comments) => {
+                    const commentCountBeforeDelete= comments.length;
+
+                    expect(commentCountBeforeDelete).toBe(1);
+
+                    request.post( `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`, 
+                    (err, res, body) => {
+                        expect(res.statusCode).toBe(302);
+                        Comment.all()
+                        .then((comments) => {
+                            expect(err).toBeNull();
+                            expect(comments.length).toBe(commentCountBeforeDelete - 1);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
     });
 
 });
