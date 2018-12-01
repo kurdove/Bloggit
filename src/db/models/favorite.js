36a9f2d7d4b9b73556626d9f95093c51d56809a2
Evgheni Curdov <kurdov.e@gmail.com>
2018-12-01 14:19:16 -0800

    assignment-16-public complete

--- src/db/models/favorite.js
+++ src/db/models/favorite.js
@@ -21,6 +21,17 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       onDelete: "CASCADE"
     });
+
+    Favorite.addScope("lastFiveFor", (userId) => {
+      return {
+        include: [{
+          model: models.Post
+        }],
+        where: { userId: userId},
+        limit: 5,
+        order: [["createdAt", "DESC"]]
+      }
+    });
   };
   return Favorite;
 };
--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -2,6 +2,7 @@ const User = require("./models").User;
 const bcrypt = require("bcryptjs");
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require('./models').Favorite;
 
 module.exports = {
   createUser(newUser, callback){
@@ -35,7 +36,11 @@ module.exports = {
           Comment.scope({method: ["lastFiveFor", id]}).all()
           .then((comments) => {
             result["comments"] = comments;
-            callback(null, result);
+            Favorite.scope({method: ['lastFiveFor', id]}).all()
+            .then((favorites) => {
+                result['favorites'] = favorites;
+                callback(null, result);
+            })
           })
           .catch((err) => {
             callback(err);
